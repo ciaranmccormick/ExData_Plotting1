435ecf91c691738a84967cb3f5f9f91bef4765b8
Ciaran McCormick <ciaranmccormick@gmail.com>
2014-07-08 18:28:41 +0100

    Tidy up some variable names

--- plot1.R
+++ plot1.R
@@ -9,10 +9,12 @@ dat <- read.table(fileName, header=TRUE, sep=";", na.strings="?", colClasses=c(r
 # Extract Dates and Times and create DateTime type
 dateTimeFormat <- "%Y-%m-%d %H:%M:%S"
 dates <- as.Date(dat$Date, format="%d/%m/%Y")
-dat$DateTime <- strptime(paste(dates,household$Time), dateTimeFormat)
+dat$DateTime <- strptime(paste(dates,dat$Time), dateTimeFormat)
 
 # Subset dat to get only consumption between 2007-02-01 and 2007-02-02
 dat <- subset(dat, DateTime >= as.POSIXlt('2007-02-01 00:00:00') & DateTime < as.POSIXlt('2007-02-03 00:00:00'), select = c(DateTime,Global_active_power))
+
+
 gap <- dat$Global_active_power
 png(file="plot1.png", width=480, height=480, units="px") # Open png device
 # Plot histogram with title and labels
